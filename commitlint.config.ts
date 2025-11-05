32a165e4e3138ada6237157d6acb7165c11475fa
Timmi6790 <13711365+Timmi6790@users.noreply.github.com>
2025-11-05 16:59:38 +0100

    refactor: minor fixes

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,7 @@ import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => message.startsWith('chore: bump')], // Ignore dependabot commits
+  ignores: [(message: string): boolean => message.startsWith('chore: bump')], // Ignore dependabot commits
 }
 
 export default Configuration
